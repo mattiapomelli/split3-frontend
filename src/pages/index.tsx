c242cb1b5fa5d863536dce2e72343f94d34470fd
mattiapomelli <mattiapomelli@gmail.com>
2023-09-01 23:36:26 +0200

    Setup request payment

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,13 @@
+import {
+  approveErc20,
+  hasErc20Approval,
+  hasSufficientFunds,
+  payRequest,
+} from "@requestnetwork/payment-processor";
 import { RequestNetwork } from "@requestnetwork/request-client.js";
 import { Types, Utils } from "@requestnetwork/request-client.js";
 import { Web3SignatureProvider } from "@requestnetwork/web3-signature";
-// import { providers } from "ethers";
-import React from "react";
+import React, { useState } from "react";
 import { useAccount } from "wagmi";
 // import { alchemyProvider } from "wagmi/providers/alchemy";
 
@@ -11,6 +16,8 @@ import { Button } from "@components/basic/button";
 
 import type { NextPage } from "next";
 
+const network = "goerli";
+const tokenAddress = "0x0FA8781a83E46826621b3BC094Ea2A0212e71B23"; // USDC on Mumbai
 const zeroAddress = "0x0000000000000000000000000000000000000000";
 
 interface CreateRequestParameters {
@@ -29,8 +36,8 @@ const getCreateRequestParameters = ({
       // The currency in which the request is denominated
       currency: {
         type: Types.RequestLogic.CURRENCY.ERC20,
-        value: "0xBA62BCfcAaFc6622853cca2BE6Ac7d845BC0f2Dc",
-        network: "goerli",
+        value: tokenAddress,
+        network: network,
       },
 
       // The expected amount as a string, in parsed units, respecting `decimals`
@@ -57,7 +64,7 @@ const getCreateRequestParameters = ({
     paymentNetwork: {
       id: Types.Extension.PAYMENT_NETWORK_ID.ERC20_FEE_PROXY_CONTRACT,
       parameters: {
-        paymentNetworkName: "goerli",
+        paymentNetworkName: network,
         paymentAddress: receiverAddress,
         feeAddress: zeroAddress,
         feeAmount: "0",
@@ -81,6 +88,7 @@ const getCreateRequestParameters = ({
 
 const Home: NextPage = () => {
   const { address } = useAccount();
+  const [requestId, setRequestId] = useState<string | null>(null);
 
   const createRequest = async () => {
     if (!address) return;
@@ -113,17 +121,72 @@ const Home: NextPage = () => {
 
     const requestCreateParameters = getCreateRequestParameters({
       payerAddress: address,
-      receiverAddress: "0x7eB023BFbAeE228de6DC5B92D0BeEB1eDb1Fd567",
+      receiverAddress: "0x0F45421E8DC47eF9edd8568a9D569b6fc7Aa7AC6",
       amount: "1000000000000000000",
     });
     const request = await requestClient.createRequest(requestCreateParameters);
 
+    setRequestId(request.requestId);
+
     console.log("Request: ", request);
+  };
+
+  const onPayRequest = async (requestId: string) => {
+    if (!address) return;
+
+    // const provider = window.ethereum;
+    // const web3SignatureProvider = new Web3SignatureProvider(provider);
+
+    const requestClient = new RequestNetwork({
+      nodeConnectionConfig: {
+        baseURL: "https://goerli.gateway.request.network/",
+      },
+      // signatureProvider: web3SignatureProvider,
+    });
+
+    const request = await requestClient.fromRequestId(requestId);
+    const requestData = request.getData();
+
+    console.log("Request data: ", requestData);
+
+    // @ts-ignore
+    // const provider = new providers.Web3Provider(window.ethereum);
+    const payerHasSufficientFunds = await hasSufficientFunds(
+      requestData,
+      address,
+      // {
+      //   provider: provider,
+      // },
+    );
+
+    console.log("Payer has sufficient funds: ", payerHasSufficientFunds);
+
+    const payerHasErc20Approval = await hasErc20Approval(requestData, address);
+    console.log("Payer has Erc20 approval: ", payerHasErc20Approval);
+
+    if (!payerHasErc20Approval) {
+      const approvalTx = await approveErc20(requestData);
+      await approvalTx.wait();
+    }
+
+    const tx = await payRequest(requestData);
+    await tx.wait();
+
+    const request2 = await requestClient.fromRequestId(requestId);
+    const requestData2 = request2.getData();
+
+    console.log("Request data: ", requestData2);
   };
 
   return (
     <div>
       <Button onClick={createRequest}>Create request</Button>
+      {requestId && (
+        <div>
+          <h3>Request id: {requestId}</h3>
+          <Button onClick={() => onPayRequest(requestId)}>Pay request</Button>
+        </div>
+      )}
     </div>
   );
 };
