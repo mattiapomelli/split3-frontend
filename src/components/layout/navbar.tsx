68f0ee8e60dbacca4606e7cfb8bc73bf41941489
mattiapomelli <mattiapomelli@gmail.com>
2023-09-02 23:09:08 +0200

    Add celo network

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -4,6 +4,7 @@ import { useAccount } from "wagmi";
 
 import { Logo } from "@components/basic/logo";
 import { ThemeToggle } from "@components/basic/theme-toggle";
+import { ChainSwitch } from "@components/chain-switch";
 
 import { WalletStatus } from "../wallet/wallet-status";
 
@@ -13,12 +14,13 @@ export const Navbar = () => {
   const { isConnecting, isReconnecting } = useAccount();
 
   return (
-    <header className="flex h-20 items-center">
+    <header className="flex h-20 items-center" suppressHydrationWarning>
       <Container className="flex w-full items-center justify-between">
         <Logo />
         {!isReconnecting && !isConnecting && (
-          <div className="flex items-center gap-4 duration-100 animate-in fade-in">
+          <div className="animate-in fade-in flex items-center gap-4 duration-100">
             <ThemeToggle />
+            <ChainSwitch />
             <WalletStatus />
           </div>
         )}
--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,21 +1,31 @@
-import { goerli, hardhat, polygon } from "wagmi/chains";
+import { celoAlfajores, goerli, hardhat, polygon } from "wagmi/chains";
 
+import HardhatIcon from "@icons/hardhat.svg";
+import PolygonIcon from "@icons/polygon.svg";
 import { env } from "env.mjs";
 
 export type ChainMap = { [chainId: number]: string };
 
-const getChain = () => {
+const getChains = () => {
   switch (env.NEXT_PUBLIC_CHAIN) {
     case "localhost":
-      return hardhat;
+      return [hardhat, goerli, celoAlfajores];
     case "testnet":
-      return goerli;
-    // return polygonMumbai;
+      return [goerli, celoAlfajores];
     case "mainnet":
-      throw polygon;
+      throw [polygon];
     default:
       throw new Error("Invalid NEXT_PUBLIC_CHAIN value");
   }
 };
 
-export const CHAIN = getChain();
+export const CHAINS = getChains();
+
+type Icon = (className: { className?: string }) => JSX.Element;
+
+export const CHAIN_ICON: { [chainId: number]: Icon } = {
+  [hardhat.id]: HardhatIcon,
+  [goerli.id]: PolygonIcon,
+  [polygon.id]: PolygonIcon,
+  [celoAlfajores.id]: PolygonIcon,
+};
