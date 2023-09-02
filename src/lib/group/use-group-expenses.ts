f6da0820cd2ba480e98bb42898bc3ba183cb521c
lim0n3 <simonestaffa96@gmail.com>
2023-09-02 11:18:45 +0200

    Create group expenses

--- src/lib/group/use-group-expenses.ts
+++ src/lib/group/use-group-expenses.ts
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { supabaseClient } from "../../app/db";
+
 import { getGroupExpenses } from "../../app/db/group_expenses";
 interface UseGroupExpensesOptions {
   groupId: number;
