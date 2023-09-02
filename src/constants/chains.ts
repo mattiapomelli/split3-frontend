2b341c3d287f081d4ff8b4dc4b1acb8dcdc83aef
mattiapomelli <mattiapomelli@gmail.com>
2023-09-02 23:13:17 +0200

    Add mantle

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -6,12 +6,31 @@ import { env } from "env.mjs";
 
 export type ChainMap = { [chainId: number]: string };
 
+export const mantleTestnet = {
+  id: 5001,
+  name: "Mantle",
+  network: "mantle-testnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "Bit",
+    symbol: "BIT",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.testnet.mantle.xyz"],
+    },
+    public: {
+      http: ["https://rpc.testnet.mantle.xyz"],
+    },
+  },
+};
+
 const getChains = () => {
   switch (env.NEXT_PUBLIC_CHAIN) {
     case "localhost":
-      return [hardhat, goerli, celoAlfajores];
+      return [hardhat, goerli, celoAlfajores, mantleTestnet];
     case "testnet":
-      return [goerli, celoAlfajores];
+      return [goerli, celoAlfajores, mantleTestnet];
     case "mainnet":
       throw [polygon];
     default:
@@ -28,4 +47,5 @@ export const CHAIN_ICON: { [chainId: number]: Icon } = {
   [goerli.id]: PolygonIcon,
   [polygon.id]: PolygonIcon,
   [celoAlfajores.id]: PolygonIcon,
+  [mantleTestnet.id]: PolygonIcon,
 };
